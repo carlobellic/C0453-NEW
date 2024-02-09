f26e331db868261698a88415479cc99000cb1989
Carl O'Brien <100185948@student.burnley.ac.uk>
2024-02-09 10:09:09 +0000

    Added a menu which allows the user to select whether to convert from miles to feet, or feet to miles

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -2,13 +2,69 @@
 
 namespace ConsoleAppProject.App01
 {
-    /// <summary>
-    /// Please describe the main features of this App
-    /// </summary>
-    /// <author>
-    /// Derek version 0.1
-    /// </author>
     public class DistanceConverter
     {
+        public const int FEET_IN_MILES = 5280;
+        public const int MILES_IN_FEET = 1;
+        private double miles;
+        private double feet;
+
+        public void Run()
+        {
+            bool continueExecution = true;
+
+            while (continueExecution)
+            {
+                OutPutHeading();
+                Console.WriteLine("1. Convert Miles to Feet");
+                Console.WriteLine("2. Convert Feet to Miles");
+                Console.WriteLine("3. Quit");
+                Console.Write("Please choose an option: ");
+
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        ConvertMilesToFeet();
+                        break;
+                    case "2":
+                        ConvertFeetToMiles();
+                        break;
+                    case "3":
+                        continueExecution = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please choose again.");
+                        break;
+                }
+            }
+        }
+
+        private void OutPutHeading()
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("    Distance Converter Menu     ");
+            Console.WriteLine("       by Carl O'Brien            ");
+            Console.WriteLine("--------------------------------");
+        }
+
+        private void ConvertMilesToFeet()
+        {
+            Console.Write("Please enter the number of miles: ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+            feet = miles * FEET_IN_MILES;
+            Console.WriteLine($"{miles} miles is equal to {feet} feet.");
+        }
+
+        private void ConvertFeetToMiles()
+        {
+            Console.Write("Please enter the number of feet: ");
+            string value = Console.ReadLine();
+            feet = Convert.ToDouble(value);
+            miles = feet / FEET_IN_MILES;
+            Console.WriteLine($"{feet} feet is equal to {miles} miles.");
+        }
     }
 }
