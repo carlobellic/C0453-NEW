b0abda092a6f14778bc52158899472a28603db5e
carlobellic <145754932+carlobellic@users.noreply.github.com>
2024-05-24 10:21:51 +0100

    Update Program.cs
    Updated year on title.

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleAppProject
 
             Console.WriteLine();
             Console.WriteLine(" =================================================");
-            Console.WriteLine("    BNU CO453 Applications Programming 2022-2023! ");
+            Console.WriteLine("    BNU CO453 Applications Programming 2023-2024! ");
             Console.WriteLine(" =================================================");
             Console.WriteLine();
 // Allows the user to select between the 4 different programs available.
