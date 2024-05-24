154378c268b6cac07c3812d83fa13c1b9724debc
carlobellic <145754932+carlobellic@users.noreply.github.com>
2024-05-24 10:02:33 +0100

    Update PhotoPost.cs

--- ConsoleAppProject/App04/PhotoPost.cs
+++ ConsoleAppProject/App04/PhotoPost.cs
@@ -13,7 +13,7 @@ namespace ConsoleAppProject.App04
             PhotoFilename = photoFilename;
             Caption = caption;
         }
-
+// Similar to message post, this just uses the ability to add the filename of the photo instead of a message, as well as the author, likes and comments, as well as being able to add a caption to the image.
         public override void Display()
         {
             Console.WriteLine($"Photo Post by {Author}");
