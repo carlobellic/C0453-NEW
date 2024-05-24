f1709681c877eff02bf69adca1c86c8a6c08becd
carlobellic <145754932+carlobellic@users.noreply.github.com>
2024-05-24 10:01:45 +0100

    Update MessagePost.cs

--- ConsoleAppProject/App04/MessagePost.cs
+++ ConsoleAppProject/App04/MessagePost.cs
@@ -11,7 +11,7 @@ namespace ConsoleAppProject.App04
         {
             Message = message;
         }
-
+// This code literally just displays the post when it's been posted, so it displays who wrote the post, when it was posted, the message posted, amount of likes and comments.
         public override void Display()
         {
             Console.WriteLine($"Message Post by {Author}");
